ffa329061706ed60b0375f6dd66a259500e27e79
Tau Andrade <nullvoxel@gmail.com>
2022-03-29 05:39:40 +1100

    unused import

--- src/me/taucu/reconnect/util/provider/TitleViewEntry.java
+++ src/me/taucu/reconnect/util/provider/TitleViewEntry.java
@@ -3,7 +3,6 @@ package me.taucu.reconnect.util.provider;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 import net.md_5.bungee.api.ChatColor;
 
