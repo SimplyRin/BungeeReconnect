a1525331eb1251ff63450f6ed892a2ea88d7ccde
Tau <nullvoxel@gmail.com>
2022-06-17 11:40:02 +1000

    Add Music API

--- src/me/taucu/reconnect/util/provider/Music.java
+++ src/me/taucu/reconnect/util/provider/Music.java
@@ -3,20 +3,15 @@ package me.taucu.reconnect.util.provider;
 import dev.simplix.protocolize.api.SoundCategory;
 import dev.simplix.protocolize.api.player.ProtocolizePlayer;
 import dev.simplix.protocolize.data.Sound;
+import lombok.Data;
 
+@Data
 public class Music {
 
     final Sound music;
     final SoundCategory category;
     final float volume;
     final float pitch;
-
-    public Music(Sound music, SoundCategory category, float volume, float pitch) {
-        this.music = music;
-        this.category = category;
-        this.volume = volume;
-        this.pitch = pitch;
-    }
 
     public void play(ProtocolizePlayer player) {
         player.playSound(music, category, volume, pitch);
--- src/me/taucu/reconnect/util/provider/MusicProvider.java
+++ src/me/taucu/reconnect/util/provider/MusicProvider.java
@@ -3,6 +3,7 @@ package me.taucu.reconnect.util.provider;
 import dev.simplix.protocolize.api.Protocolize;
 import dev.simplix.protocolize.api.player.ProtocolizePlayer;
 import dev.simplix.protocolize.api.providers.ProtocolizePlayerProvider;
+import lombok.Getter;
 import me.taucu.reconnect.packets.ClientboundStopSoundPacket;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
@@ -14,7 +15,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class MusicProvider {
 
     final ProtocolizePlayerProvider provider = Protocolize.playerProvider();
-    final List<Music> musics;
+    @Getter final List<Music> musics;
 
     public MusicProvider(Collection<Music> musics) {
         this.musics = new ArrayList<>(musics);
